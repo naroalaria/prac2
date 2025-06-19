ea2b5453c5e58d1b5d847f94b5322958b0fb236f
naroalaria <naroa.laria@alumni.mondragon.edu>
2025-06-19 17:51:07 +0200

    api funcional

---
+++ plumber.R
@@ -0,0 +1,10 @@
+
+  #* @apiTitle Plumber Example API
+  #* #* @apiDescription Plumber example description.
+  #* #* @param msg The message to echo
+  #* #* @get /echo
+
+  function(Minutes) {
+    minutos <- as.numeric(datos$Minutes)
+    return(hist(datos$Minutes))
+}
